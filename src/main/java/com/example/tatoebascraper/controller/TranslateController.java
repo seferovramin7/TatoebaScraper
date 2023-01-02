974faaa05979368b65904536f96cdf9e7cf54fd7
seferovramin7 <seferovramin7@gmail.com>
2023-01-02 16:56:03 +0400

    languages endpoint added

--- src/main/java/com/example/tatoebascraper/controller/TranslateController.java
+++ src/main/java/com/example/tatoebascraper/controller/TranslateController.java
@@ -2,9 +2,9 @@ package com.example.tatoebascraper.controller;
 
 import com.example.tatoebascraper.dto.ResponseDto;
 import com.example.tatoebascraper.service.impl.RestService;
+import com.example.tatoebascraper.util.LangSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +13,22 @@ import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("translate")
 public class TranslateController {
     private final RestService restService;
+    private final LangSource langSource;
 
-    @GetMapping
+    @GetMapping("translate")
     public ResponseDto translate(@RequestParam String from, @RequestParam String to, @RequestParam String word) throws IOException {
         HashMap translated = restService.findTranslate(from, to, word);
         return ResponseDto.builder()
                 .fromLang(translated.keySet().stream().findFirst().get().toString())
                 .toLang(translated.values().stream().findFirst().get().toString())
                 .build();
+    }
+
+    @GetMapping("languages")
+    public HashMap<String, String> languages() throws IOException {
+        HashMap<String, String> data = langSource.retrieveData();
+        return data;
     }
 }
